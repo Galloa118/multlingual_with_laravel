a50f3112fb1edcc68e4fbd8be3a0aebb3f956685
Galloa118 <pewallin0527@gmail.com>
2017-05-06 15:38:32 +0200

    Finished destroy.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -102,6 +102,8 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        $article->delete();
+
+        return redirect()->route('articles.index');
     }
 }
